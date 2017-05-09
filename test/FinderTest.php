e3621dfa517edd8726cf8b7f213beae1bc9faf4e
jeff <jfrancois.escaich@gmail.com>
2017-05-09 16:49:32 +0200

    ajout des tests unitaires pour de vrai

--- test/FinderTest.php
+++ test/FinderTest.php
@@ -11,6 +11,8 @@ class FinderTest extends \PHPUnit_Framework_TestCase
     public function testOk() {
 //        $this->assertTrue(true);
 
+
+// AJOUT D'UN COMM HISTOIRE DE
         $a = new Finder('bannane', ["kiwi", "banane"]);
 //        echo $a->First();
 //        $a->First() == 'banana'; 
